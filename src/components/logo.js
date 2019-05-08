d049e587db37a5648ab28618f99a726050ab10ba
GeoHernandez27 <leopardo2@gmail.com>
2019-05-08 10:01:53 -0600

    set up redux form in search bar component

---
+++ src/components/logo.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Logo = () => {
+        return (
+            <div>
+                <div className="logo-main">
+                    <img src="/assets/ds_circle_logo.png" alt="daily smary ui image"/>
+                </div>
+            </div>
+        )
+    };
+
+export default Logo;
